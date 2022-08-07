e781320320a44b9c4338652a1f607e5d3f0cd101
Vishal Singh <72917258+VishalSingh-07@users.noreply.github.com>
2022-08-07 11:50:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,95 +1,84 @@
-var gamepattern=[];
-var userClickedPattern=[];
-var buttonColours=['red','blue','green','yellow'];
-var level = 0;
-var started=false;
+var gamepattern = []
+var userClickedPattern = []
+var buttonColours = ["red", "blue", "green", "yellow"]
+var level = 0
+var start = false;
+var started = false;
 
-function nextSequence()
-{
-      userClickedPattern = [];
-      level++;
-      $("#level-title").text("Level " + level)
-      var randomnumber=Math.floor(Math.random()*4);
-      var randomChosenColour=buttonColours[randomnumber];
-      gamepattern.push(randomChosenColour);
-      
-      $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-      playsound(randomChosenColour);
+function startGame() {
+	start = true
+	setTimeout(() => {
+		nextSequence()
+	}, 500)
 }
-$(".btn").on("click",function()
-{
-      var userChosenColour=$(this).attr("id");
-      userClickedPattern.push(userChosenColour);
-      playsound(userChosenColour);
-      animatePress(userChosenColour);
-      checkAnswer(userClickedPattern.length-1);
+function nextSequence() {
+	userClickedPattern = []
+	level++
+	$("#level-title").text("Level " + level)
+	var randomnumber = Math.floor(Math.random() * 4)
+	var randomChosenColour = buttonColours[randomnumber]
+	gamepattern.push(randomChosenColour)
 
-
-});
-function playsound(name)
-{
-      var audio = new Audio("./sounds/" + name + ".mp3")
-      audio.play()
+	$("#" + randomChosenColour)
+		.fadeIn(100)
+		.fadeOut(100)
+		.fadeIn(100)
+	playsound(randomChosenColour)
 }
-function animatePress(currentColour)
-{
-      $("."+currentColour).addClass("pressed");
-      setTimeout(function(){
-            $("." + currentColour).removeClass("pressed")
-
-      },50);
-
+$(".button").on("click", function (event) {
+	if (start) {
+		var userChosenColour = $(this).attr("id")
+		userClickedPattern.push(userChosenColour)
+		playsound(userChosenColour)
+		animatePress(userChosenColour)
+		checkAnswer(userClickedPattern.length - 1)
+	}
+})
+function playsound(name) {
+	var audio = new Audio("./sounds/" + name + ".mp3")
+	audio.play()
 }
-$(document).keydown(function(){
-      
-      if(!started)
-      {
-            
-            $("#level-title").text("Level " + level);
-            nextSequence();
-            started=true;
-
-      }
-      
-});
-
-function checkAnswer(currentLevel)
-{
-      if(gamepattern[currentLevel] === userClickedPattern[currentLevel])
-      {
-            if(userClickedPattern.length === gamepattern.length)
-            {
-                  setTimeout(function()
-                  {
-                        nextSequence();
-
-                  },1000);
-            }
-      }
-      else
-      {
-            playsound("wrong");
-            $("body").addClass("game-over");
-            $("#level-title").text("Game Over, Press Any Key to Restart");
-            setTimeout(function () {
-            $("body").removeClass("game-over")
-            }, 200);
-            startOver();
-
-      }
-
+function animatePress(currentColour) {
+	$("." + currentColour).addClass("pressed")
+	setTimeout(function () {
+		$("." + currentColour).removeClass("pressed")
+	}, 50)
 }
-function startOver()
-{
-      level=0;
-      gamepattern=[];
-      started=false;
+$(document).keydown(function () {
+	if (!started) {
+		$("#level-title").text("Level " + level)
+		nextSequence()
+		started = true
+		start = true
+	}
+})
 
+function checkAnswer(currentLevel) {
+	if (gamepattern[currentLevel] === userClickedPattern[currentLevel]) {
+		if (userClickedPattern.length === gamepattern.length) {
+			setTimeout(function () {
+				nextSequence()
+			}, 1000)
+		}
+	} else {
+		playsound("wrong")
+		$("body").addClass("game-over")
+		$("#level-title").text("Game Over, Press Any Key to Restart")
+		setTimeout(function () {
+			$("body").removeClass("game-over")
+		}, 200)
+		startOver()
+	}
+}
+function startOver() {
+	level = 0
+	gamepattern = []
+	started = false
+	start = false
 }
 
 // Sound on Footer
 document.querySelector(".footer").addEventListener("click", function () {
-  var audio = new Audio("./sounds/Footer.wav")
-  audio.play()
-});
-
+	var audio = new Audio("./sounds/Footer.wav")
+	audio.play()
+})
